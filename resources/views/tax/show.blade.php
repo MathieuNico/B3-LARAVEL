b09b9894f86fa105278ed198938063f5cac19b50
MathieuNico <mathieu.nicolas49@gmail.com>
2025-02-17 15:00:06 +0100

    test ci

--- resources/views/tax/show.blade.php
+++ resources/views/tax/show.blade.php
@@ -5,8 +5,6 @@
     @if ($revenutotal < 15000)
         <p>Tu es un régime micro-foncier, tu dois donc inscrire {{$revenutotal}} dans la case case 4 BE déclaration n°2042</p>
         <p> Sur quel montant sera tu imposé ? {{$revenutotal * 0.70}}</p>
-        <p>voila</p>
-        <p>Testest</p>
     @else
         <p>Tu es un régime réél, tu dois inscrire {{$revenutotal}} case 4 BA déclaration n°2044</p>
         <p> Sur quel montant sera tu imposé ? {{$revenutotal}}</p>
